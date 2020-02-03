c0e711ec16e8ef0006ccd735f722dbff917b1545
emmakravets <veseladove@gmail.com>
2020-02-03 19:46:37 +0300

    Improves func

--- js/hashtags.js
+++ js/hashtags.js
@@ -5,7 +5,7 @@ var HASHTAGS_COUNT_LIMIT = 5;
 var SEPARATOR = ' ';
 var HASH_SYMBOL = '#';
 
-var validateHashtags = function (value) {
+/* var validateHashtags = function (value) {
   var hashtags = value.split(SEPARATOR);
 
   for (var i = 0; i < hashtags.length; i++) {
@@ -25,6 +25,35 @@ var validateHashtags = function (value) {
       hashtagsInputElement.setCustomValidity('Один и тот же хэштег не может быть использован дважды');
     }
   }
+};
+*/
+
+var validateHashtag = function (tag) {
+  if (tag[0] !== HASH_SYMBOL) {
+    return 'Хэштег должен начинаться с # (решётка)';
+  } else if (tag === HASH_SYMBOL && tag.length === 1) {
+    return 'Хэштег не может состоять только из одной решётки';
+  } else if (tag.length > HASHTAG_LENGTH_LIMIT) {
+    return 'Хэштег не должен превышать ' + HASHTAG_LENGTH_LIMIT + ' символов';
+  } else if (tag.split(HASH_SYMBOL).length > 2) {
+    return 'Хэштеги должны разделяться пробелами';
+  } else if (tag.slice(1).find(function (item) {
+    return tag === item;
+  })) {
+    return 'Один и тот же хэштег не может быть использован дважды';
+  }
+  return '';
+};
+
+var validateHashtags = function (value) {
+  var hashtags = value.split(SEPARATOR);
+  var validationMessage;
+  if (hashtags.length > HASHTAGS_COUNT_LIMIT) {
+    validationMessage = 'Нельзя указать больше пяти хэштегов';
+  } else {
+    validationMessage = hashtags.find(validateHashtag);
+  }
+  hashtagsInputElement.setCustomValidity(validationMessage);
 };
 
 var hashtagsInputElement = document.querySelector('.text__hashtags');
